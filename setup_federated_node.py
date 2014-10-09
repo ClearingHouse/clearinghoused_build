009fe02f91fc2050953d9ae3ed8c5f5978842213
BtcDrak <btcdrak@gmail.com>
2014-10-10 00:21:09 +0100

    Update for Viacoin Core 0.10.4

--- setup_federated_node.py
+++ setup_federated_node.py
@@ -241,9 +241,9 @@ def do_bitcoind_setup(run_as_user, branch, base_path, dist_path, run_mode):
     bitcoind_rpc_password_testnet = pass_generator()
     
     #Install bitcoind
-    BITCOIND_VER = "0.10.3"
+    BITCOIND_VER = "0.10.4"
     runcmd("rm -rf /tmp/viacoind.tar.gz /tmp/viacoin-%s-linux" % BITCOIND_VER)
-    runcmd("wget -O /tmp/viacoind.tar.gz https://github.com/viacoin/viacoin/releases/download/v%s/viacoin-%s-utxo.tar.gz" % (BITCOIND_VER, BITCOIND_VER))
+    runcmd("wget -O /tmp/viacoind.tar.gz https://github.com/viacoin/viacoin/releases/download/v%s/viacoin-%s-linux.tar.gz" % (BITCOIND_VER, BITCOIND_VER))
     runcmd("tar -C /tmp -zxvf /tmp/viacoind.tar.gz")
     runcmd("cp -af /tmp/viacoin-%s-linux/bin/64/viacoind /usr/local/bin" % BITCOIND_VER)
     runcmd("cp -af /tmp/viacoin-%s-linux/bin/64/viacoin-cli /usr/local/bin" % BITCOIND_VER)
